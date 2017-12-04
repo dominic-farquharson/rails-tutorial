0d188c877586288258d0873ac2ee2f294b6ccfed
Dominic Farquharson <farquharsondominic@gmail.com>
2017-12-03 21:13:24 -0500

    add create action to articles. add view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,13 @@
 class ArticlesController < ApplicationController
   def new
   end
+
+  # create action /articles/create
+  def create
+    puts "-------- "
+    puts params
+    render plain: params[:article].inspect
+    # json => render json: params[:article]
+
+  end
 end
